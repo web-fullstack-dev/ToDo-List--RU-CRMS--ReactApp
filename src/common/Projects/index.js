13e8a4060d40b17ae6b9acf2a45ba66b7b511d6e
Nikita Rysiev <74302800+web-fullstack-dev@users.noreply.github.com>
2024-09-07 22:37:28 +0200

    Update index.js

--- src/common/Projects/index.js
+++ src/common/Projects/index.js
@@ -5,12 +5,12 @@ import { Links, Link, Button } from "./styled";
 const Projects = () => (
   <Links>
     <Clock />
-    <Link href={"https://mr-front-end-dev.github.io/ToDo-List--EN-CRMS--ReactApp"}><Button>EN</Button></Link>
-    <Link href={"https://mr-front-end-dev.github.io/ToDo-List--DE-CRMS--ReactApp"}><Button>DE</Button></Link>
-    <Link href={"https://mr-front-end-dev.github.io/ToDo-List--ES-CRMS--ReactApp"}><Button>ES</Button></Link>
-    <Link href={"https://mr-front-end-dev.github.io/ToDo-List--FR-CRMS--ReactApp"}><Button>FR</Button></Link>
-    <Link href={"https://mr-front-end-dev.github.io/ToDo-List--PL-CRMS--ReactApp"}><Button>PL</Button></Link>
-    <Link href={"https://mr-front-end-dev.github.io/ToDo-List--UA-CRMS--ReactApp"}><Button>UA</Button></Link>
+    <Link href={"https://web-fullstack-dev.github.io/ToDo-List--EN-CRMS--ReactApp"}><Button>EN</Button></Link>
+    <Link href={"https://web-fullstack-dev.github.io/ToDo-List--DE-CRMS--ReactApp"}><Button>DE</Button></Link>
+    <Link href={"https://web-fullstack-dev.github.io/ToDo-List--ES-CRMS--ReactApp"}><Button>ES</Button></Link>
+    <Link href={"https://web-fullstack-dev.github.io/ToDo-List--FR-CRMS--ReactApp"}><Button>FR</Button></Link>
+    <Link href={"https://web-fullstack-dev.github.io/ToDo-List--PL-CRMS--ReactApp"}><Button>PL</Button></Link>
+    <Link href={"https://web-fullstack-dev.github.io/ToDo-List--UA-CRMS--ReactApp"}><Button>UA</Button></Link>
     <Link href={"#"}><Button deepSkyBlue>🌓</Button></Link>
   </Links>
 )
